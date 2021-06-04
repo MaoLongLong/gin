cc119dfe6833baacefd0d999ad9a0b052fa022c1
MaoLongLong <382084620@qq.com>
2021-06-04 18:38:12 +0800

    impl group router

--- gin.go
+++ gin.go
@@ -1,27 +1,37 @@
 package gin
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 type HandlerFunc func(*Context)
 
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method, path string, handler HandlerFunc) {
-	engine.router.addRoute(method, path, handler)
+func (group *RouterGroup) addRoute(method, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) GET(path string, handler HandlerFunc) {
-	engine.addRoute("GET", path, handler)
+func (group *RouterGroup) GET(path string, handler HandlerFunc) {
+	group.addRoute("GET", path, handler)
 }
 
-func (engine *Engine) POST(path string, handler HandlerFunc) {
-	engine.addRoute("POST", path, handler)
+func (group *RouterGroup) POST(path string, handler HandlerFunc) {
+	group.addRoute("POST", path, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
@@ -31,4 +41,22 @@ func (engine *Engine) Run(addr string) (err error) {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := newContext(w, r)
 	engine.router.handle(c)
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
